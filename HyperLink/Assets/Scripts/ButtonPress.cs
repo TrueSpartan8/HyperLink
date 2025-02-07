b2ca0d985f3f979c39dd361b4841efd5b0b09ba4
TrueSpartan8 <theodore.acciaccaferri@gmail.com>
2025-02-06 19:08:06 -0500

    BUTTON!!!!!!!!!!!!!

---
+++ HyperLink/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonPress : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "block" || collider.gameObject.tag == "Player")
+        {
+            animator.SetBool("isButtonPressed", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("block") || collider.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("isButtonPressed", false);
+        }
+    }
+}
