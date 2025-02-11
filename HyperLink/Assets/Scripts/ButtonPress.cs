1926f2dad8477c8f4fa6665c1b38da709e209479
daronYaziciyan <daron76y@my.yorku.ca>
2025-02-10 22:19:34 -0500

    Updated Button and Door scripts
    - moved teleporter 0-B as a child of 0-A
    --- from now on, add all B-teleporters as children of A-teleporters for organization
    - modified door and button scripts to allow us to drag and drop multiple child button objects to the script component. All buttons must be pressed to open the door.

--- HyperLink/Assets/Scripts/ButtonPress.cs
+++ HyperLink/Assets/Scripts/ButtonPress.cs
@@ -1,21 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ButtonPress : MonoBehaviour
 {
     private Animator animator;
+    private Boolean isPressed = false; //the state of this button object
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    //public getter method for the button state
+    public Boolean GetIsPressed() {
+        return isPressed;
+    }
+
     void OnTriggerStay2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "block" || collider.gameObject.tag == "Player")
         {
+            isPressed = true;
             animator.SetBool("isButtonPressed", true);
-            BroadcastMessage("openDoor");
+            SendMessageUpwards("CheckButtonState", null, SendMessageOptions.DontRequireReceiver); //invoke any parent objects that have the CheckButtonState() method, and ignore when there are none
         }
     }
 
@@ -23,8 +31,9 @@ public class ButtonPress : MonoBehaviour
     {
         if (collider.gameObject.CompareTag("block") || collider.gameObject.CompareTag("Player"))
         {
+            isPressed = false;
             animator.SetBool("isButtonPressed", false);
-            BroadcastMessage("closeDoor");
+            SendMessageUpwards("CheckButtonState", null, SendMessageOptions.DontRequireReceiver); //invoke any parent objects that have the CheckButtonState() method, and ignore when there are none
         }
     }
 }
--- HyperLink/Assets/Scripts/doorScript.cs
+++ HyperLink/Assets/Scripts/doorScript.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class doorScript : MonoBehaviour
 {
     private Animator animator;
+    public GameObject[] Buttons; //all the buttons that are required for opening this door
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,19 +16,24 @@ public class doorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void openDoor()
-    {
-        gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        animator.SetBool("isOpen", true);
-    }
-
-    void closeDoor()
-    {
-        gameObject.GetComponent<BoxCollider2D>().enabled = true;
-        animator.SetBool("isOpen", false);
-
+    // Check the button state of all the buttons associated with this door object. This is only called when a child button object changes state
+    void CheckButtonState() {
+        Boolean isOpen = true; //assume the door is open for now
+        foreach(GameObject button in Buttons) { //iterate through each button
+            if (!button.GetComponent<ButtonPress>().GetIsPressed()) { //if atleast one button is not pressed, this door is not open
+                isOpen = false;
+            }
+        }
+        if (isOpen) {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            animator.SetBool("isOpen", true);
+        }
+        else {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            animator.SetBool("isOpen", false);
+        }
     }
 }
