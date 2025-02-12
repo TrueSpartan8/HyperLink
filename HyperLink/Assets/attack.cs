31a9dbec64d2465052db4449aa4eb629351b32e3
TrueSpartan8 <theodore.acciaccaferri@gmail.com>
2025-02-12 15:41:05 -0500

    ATTACKS (kinda)

---
+++ HyperLink/Assets/attack.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Swing()
+    {
+        animator.SetTrigger("attack");
+        //animator.ResetTrigger("attack");
+    }
+
+}
