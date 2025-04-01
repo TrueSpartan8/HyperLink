b3d62a72080c4e0c5d77210c6ccf1a76eaac191e
TrueSpartan8 <theodore.acciaccaferri@gmail.com>
2025-03-31 20:00:38 -0400

    Enemy Tracking

---
+++ HyperLink/Assets/enemyBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float detectionDistance = 1f;
+    public float stopDistance = 0.1f;
+
+    public Transform player;
+
+    public float moveSpeed = 1f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stopDistance && Vector2.Distance(transform.position, player.position) < detectionDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) > detectionDistance)
+        {
+            transform.position = this.transform.position;
+        }
+    }
+}
