a2de8660c6695f53106ba57f7a3e5bbb7cdb5d01
TrueSpartan8 <theodore.acciaccaferri@gmail.com>
2025-03-31 20:47:54 -0400

    Enemy Animations

--- HyperLink/Assets/enemyBehaviour.cs
+++ HyperLink/Assets/enemyBehaviour.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class enemyBehaviour : MonoBehaviour
 {
     private Rigidbody2D rb;
-
     public float detectionDistance = 1f;
     public float stopDistance = 0.1f;
-
     public Transform player;
-
     public float moveSpeed = 1f;
+    private Animator animator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +17,7 @@ public class enemyBehaviour : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
@@ -27,11 +26,15 @@ public class enemyBehaviour : MonoBehaviour
     {
         if (Vector2.Distance(transform.position, player.position) > stopDistance && Vector2.Distance(transform.position, player.position) < detectionDistance)
         {
+            animator.SetBool("isMoving", true);
+            animator.SetFloat("InputX", player.position.x - transform.position.x);
+            animator.SetFloat("InputY", player.position.y - transform.position.y);
             transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
         }
         else if (Vector2.Distance(transform.position, player.position) > detectionDistance)
         {
             transform.position = this.transform.position;
+            animator.SetBool("isMoving", false);
         }
     }
 }
